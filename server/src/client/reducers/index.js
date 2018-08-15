a5fadfd54fa57e51139a3562ea5ed2a4b8cd7f6d
Aleksandar Donev <adonev@axway.com>
2018-08-15 17:44:21 +0300

    first-commit

---
+++ server/src/client/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import usersReducer from './usersReducer';
+import authReducer from './authReducer';
+import adminsReducer from './adminsReducer';
+
+
+export default combineReducers({
+  users: usersReducer,
+  auth: authReducer,
+  admins: adminsReducer
+});
